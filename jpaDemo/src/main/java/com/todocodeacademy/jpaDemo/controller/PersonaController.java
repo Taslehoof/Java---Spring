fe6dee0db95c0be8013cb235bf0a54a66014f234
Sergio Conti <seracon@gmail.com>
2024-09-09 18:22:06 -0300

    Creacion y complementacion de API
    Complementacion de la API para el sistema de JPA Demo y para poder complementar con la BDD

---
+++ jpaDemo/src/main/java/com/todocodeacademy/jpaDemo/controller/PersonaController.java
@@ -0,0 +1,57 @@
+package com.todocodeacademy.jpaDemo.controller;
+
+import com.todocodeacademy.jpaDemo.model.Persona;
+import com.todocodeacademy.jpaDemo.service.IPersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PersonaController {
+
+    @Autowired
+    private IPersonaService interPersona;
+
+    //Endpoint para obtener todas las personas
+    @GetMapping("/personas/traer")
+    public List<Persona> getPersonas(){
+        return interPersona.getPersonas();
+    }
+
+    //Endpoint para crear una nueva persona
+    @PostMapping("/personas/crear")
+    public String createStudent(@RequestBody Persona perso){
+        interPersona.savePersonas(perso);
+
+        //devuelve un String avisando si creo correctamente
+        return "La personas fue creada correctamente";
+    }
+
+    //Endpoint para dar de baja una nueva persona
+    @DeleteMapping("/personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        interPersona.deletePersona(id);
+
+        //devuelve un string avisando si elimino correctamente
+        return "La persona fue eliminada correctamente";
+    }
+
+    //Endpoint para modificar una pserona
+    @PutMapping("/personas/editar/{id_modificar}")
+    public Persona editPersona(@PathVariable Long id_original,
+                               @RequestParam(required=false, name= "id") Long nuevaId,
+                               @RequestParam(required=false, name= "nombre") String nuevoNombre,
+                               @RequestParam(required=false, name= "apellido") String nuevoApellido,
+                               @RequestParam(required=false, name= "edad") int nuevaEdad){
+
+        //Envio id original (para buscar)
+        //Envio nuevos datos para modificar
+        interPersona.editPersona(id_original, nuevaId, nuevoNombre, nuevoApellido, nuevaEdad);
+
+        //busco la persona editada para mostrarla en la response
+        Persona perso = interPersona.findPersona(id_original);
+
+        return perso;
+    }
+}
